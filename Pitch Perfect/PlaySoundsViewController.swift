46aba1d1ddba35efe2d5f5738cecfd06a522ee82
Sean Powley <skpdev@users.noreply.github.com>
2015-08-20 20:40:03 -0400

    finished project

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -13,6 +13,9 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,10 @@ class PlaySoundsViewController: UIViewController {
            // println("the filepath is empty")
             audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
             audioPlayer.enableRate = true
+        
+            audioEngine = AVAudioEngine()
+            audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
+        
         //}
         
         
@@ -54,6 +61,37 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer.stop()
     }
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+        func playAudioWithVariablePitch(pitch: Float) {
+            audioPlayer.stop()
+            audioEngine.stop()
+            audioEngine.reset()
+            
+            var audioPlayerNode = AVAudioPlayerNode()
+            audioEngine.attachNode(audioPlayerNode)
+            
+            var changePitchEffect = AVAudioUnitTimePitch()
+            changePitchEffect.pitch = pitch
+            audioEngine.attachNode(changePitchEffect)
+            
+            audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+            audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+            
+            audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+            audioEngine.startAndReturnError(nil)
+            
+            audioPlayerNode.play()
+    }
+    
+    @IBAction func playDarthVaderAudio(sender: UIButton) {
+        
+        playAudioWithVariablePitch(-1000)
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
