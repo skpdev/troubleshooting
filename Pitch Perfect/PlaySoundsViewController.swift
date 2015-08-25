8f5f93da4961eea6cd7b2176224a1694888cfaea
Sean Powley <skpdev@users.noreply.github.com>
2015-08-25 08:13:13 -0400

    final fixes

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -20,22 +20,12 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-        //    var filePathUrl = NSURL.fileURLWithPath(filePath)
-            
-            
-          //  } else {
-           // println("the filepath is empty")
             audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
             audioPlayer.enableRate = true
         
             audioEngine = AVAudioEngine()
             audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
-        
-        //}
-        
-        
-    }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -44,6 +34,8 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func slowDownSpeed(sender: UIButton) {
         audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
         audioPlayer.currentTime = 0.0
         audioPlayer.rate = 0.5
         audioPlayer.play()
@@ -52,6 +44,8 @@ class PlaySoundsViewController: UIViewController {
 
     @IBAction func speedUpSpeed(sender: UIButton) {
         audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
         audioPlayer.currentTime = 0.0
         audioPlayer.rate = 1.5
         audioPlayer.play()
